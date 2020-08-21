95e74707c4b5d6aca5e83f45fcd8f9b149a68b3a
muuris <55035659+muuris@users.noreply.github.com>
2020-08-21 16:19:27 +0300

    Bit mask for taper end time, so effectively uses ms_counter cast into 15 bits

--- speeduino/sensors.ino
+++ speeduino/sensors.ino
@@ -380,8 +380,9 @@ static inline void TPS_MAP_prediction()
       if ( (currentStatus.tpsDOT > configPage2.predictedMAPtresh) && (!currentStatus.MAPpredictActive) ) 
       {
         currentStatus.MAPpredictActive = 1;      //Set MAP predict active
-        MAPpredictEndTime = (uint16_t) ms_counter + (configPage2.predictedMAPtaper *5);  //Set end time for tapering by casting ms_counter value into uint16.
-      }                                                                                  //Configuration value is multiplier of 5 ms.
+        //Set end time for tapering by casting ms_counter value into uint16 with its MSB masked always as 0 (to prevent possible overflow during prediction event)
+        MAPpredictEndTime = (uint16_t) (ms_counter & 32767) + (configPage2.predictedMAPtaper *5);  //Configuration value is multiplier of 5 ms.
+      }
       
       //If MAP predict is active, taper the predicted value down to measured value
       //unless the measured value is greater, in which case it is used as is
@@ -389,7 +390,7 @@ static inline void TPS_MAP_prediction()
       if (currentStatus.MAPpredictActive)
       {
           //Check if taper time is elapsed and if so, reset map predict
-          if ((uint16_t) ms_counter > MAPpredictEndTime)
+          if (((uint16_t) ms_counter > MAPpredictEndTime)
           {
             currentStatus.MAPpredictActive = 0;
             return;
@@ -397,7 +398,7 @@ static inline void TPS_MAP_prediction()
 
           //Perform lookup into predicted MAP value map for RPM vs TPS value, and taper it to measured value
           predictedValue = 2 * get3DTableValue(&predictedMapTable, currentStatus.TPS, currentStatus.RPM);  //MAP values are stored in precision of 2 units (max 511 kPa values with a byte)
-          predictedValue = map(((uint16_t) ms_counter), ((uint16_t) MAPpredictEndTime - (configPage2.predictedMAPtaper*5)), MAPpredictEndTime, predictedValue, currentStatus.MAP); //Taper precision is 5 ms, so multiply by 5
+          predictedValue = map(((uint16_t) ms_counter & 32767), ((uint16_t) MAPpredictEndTime - (configPage2.predictedMAPtaper*5)), MAPpredictEndTime, predictedValue, currentStatus.MAP); //Taper precision is 5 ms, so multiply by 5
           
           //Finally sanity checks before modifying the measured value
           if ( (predictedValue > currentStatus.MAP) && (predictedValue > configPage2.mapMin) && (predictedValue <= configPage2.mapMax))
